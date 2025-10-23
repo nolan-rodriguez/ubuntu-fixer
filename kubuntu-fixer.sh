ff28fbcc5617cd306ca9938c514e4540233917d6
Nolan Rodriguez <nmrodriguez@proton.me>
2025-10-23 00:49:50 -0400

    Added kubuntu-fixer.sh and fixed a missing update command.

--- kubuntu-fixer.sh
+++ kubuntu-fixer.sh
@@ -3,9 +3,9 @@
 # Exit on Error
 set -e
 
-# Warn User
-while true; do
-    # Warning Prompt
+# Loops warning until read command gets a valid response
+while true; do 
+    # Warn user and read and record response as warning_response
     read -p 'WARNING! Proceeding will remove all snap packages and all data tied to them. Do you want to continue? [Y/n]: ' warning_response
 
     # Default to "y" if the user just presses Enter
@@ -16,7 +16,7 @@ while true; do
     # Check the user's response
     if [[ "$warning_response" == "y" || "$warning_response" == "Y" ]]; then
         echo "Proceeding with the removal of snap packages..."
-        break  # Exit the loop after proceeding
+        break  # Exit the loop and proceed
     elif [[ "$warning_response" == "n" || "$warning_response" == "N" ]]; then
         echo "Operation canceled"
         exit  # Exit the script
@@ -33,21 +33,19 @@ sudo apt update -y && sudo apt upgrade -y
 # Removing Snap Applications
 echo 'Removing Snap applications...'
 # Remove all user/application snaps (excluding bases and themes)
-for snap in $(snap list | awk 'NR>1 {print $1}' | grep -Ev '^(core[0-9]*|bare|snapd|gtk-common-themes)$'); do
-    sudo snap remove "$snap"
+while true; do
+    # List snaps and remove them one by one
+    for snap in $(snap list | awk 'NR>1 {print $1}'); do
+        echo "Removing snap: $snap"
+        sudo snap remove "$snap"
+    done
+
+    # Check if there are any snaps left
+    if [ -z "$(snap list | awk 'NR>1 {print $1}')" ]; then
+        echo "No snaps remaining."
+        break
+    fi
 done
-# Remove gtk-common-themes if present
-if snap list | grep -q gtk-common-themes; then
-    sudo snap remove gtk-common-themes
-fi
-# Remove base snaps if present
-for snap in $(snap list | awk 'NR>1 {print $1}' | grep -E '^(core[0-9]*|bare|snapd)$'); do
-    sudo snap remove "$snap"
-done
-# If no snaps left, print message
-if [ "$(snap list | wc -l)" -le 1 ]; then
-    echo 'No Snap applications installed.'
-fi
 
 # Remove Snap
 echo 'Removing Snap...'
@@ -56,14 +54,11 @@ sudo apt remove --purge snap snapd -y
 # Add nosnap.pref File to /etc/apt/preferences.d
 echo -e 'Package: snapd\nPin: release a=*\nPin-Priority: -10' | sudo tee /etc/apt/preferences.d/nosnap.pref > /dev/null
 
-# Install Gnome Software
-echo 'Installing Gnome Software'
-sudo apt install gnome-software -y
-
 # Install Flatpak
 echo 'Installing Flatpak'
 sudo apt install flatpak -y
-sudo apt install gnome-software-plugin-flatpak -y
+sudo apt install plasma-discover-backend-flatpak -y
+sudo apt install kde-config-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Upgrade System... Again
